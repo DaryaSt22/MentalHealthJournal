409fbd0efa4de8e0ced430de28d6f111c1adfc7f
Darya <ara1015@tut.by>
2025-07-07 20:16:55 +0300

    installed celery['redis'], added __init__.py, added CELERY_BROKER_URL (setting)

--- MentalHealthJournal/MentalHealthJournal/settings.py
+++ MentalHealthJournal/MentalHealthJournal/settings.py
@@ -117,6 +117,17 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
+CASHES = {
+'default': {
+    'BASKEND': 'django_redis.cache.RedisCache',
+    "LOCATION": 'redis://127.0.0.1:6379/1',
+    'OPTIONS': {
+        'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
+
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -212,3 +223,8 @@ SOCIALACCOUNT_PROVIDERS = {
         # 'GITHUB_URL': 'https://your.github-server.domain',
     }
 }
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
--- MentalHealthJournal/MentalHealthJournal/urls.py
+++ MentalHealthJournal/MentalHealthJournal/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.views.decorators.cache import cache_page
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -30,6 +31,7 @@ from rest_framework import permissions
 from users import views
 from users.views import (AccountUpdateView, HomeView, LoginFormView,
                          LogOutTemplateView, SignUpFormView)
+
 
 # schema_views = get_schema_view(
 #     openapi.Info(
@@ -48,7 +50,7 @@ from users.views import (AccountUpdateView, HomeView, LoginFormView,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(extra_context={'title': 'MentalHealthJournal'}), name='HomeView'),
+    path('', cache_page(30)(HomeView.as_view(extra_context={'title': 'MentalHealthJournal'})), name='HomeView'),
     path('login/', LoginFormView.as_view(), name='login'),
     path('sign_up/', SignUpFormView.as_view(), name='sign_up'),
     path('account/', AccountUpdateView.as_view(), name='account'),
