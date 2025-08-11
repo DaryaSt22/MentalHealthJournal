17db5ef11cc06f0287662f6236d01335bb366f1f
Darya <ara1015@tut.by>
2025-08-11 14:34:45 +0300

    added diagram

--- MentalHealthJournal/analytics/views.py
+++ MentalHealthJournal/analytics/views.py
@@ -1,3 +1,16 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from users.models import Profile
 
-# Create your views here.
+def index(request):
+    labels = []
+    data = []
+
+    queryset = Profile.objects.order_by('-stress_level')[:5]
+    for person in queryset:
+        labels.append(person.user)
+        data.append(person.stress_level)
+    return render(request, 'greeting.html', {
+        'labels': labels,
+        'data': data
+    })
+
